36dc251522782574277155f39c398af5bf4556eb
Moises Ariza <arizamoises@gmail.com>
2023-03-23 22:23:19 -0500

    Created class views for list

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    """
+        Vista alternativa a la vista de post
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     post_list = Post.published.all()
@@ -10,7 +21,7 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
-        
+
     except PageNotAnInteger:
         #Si page-number no es un integer
         posts = paginator.page(1)    
